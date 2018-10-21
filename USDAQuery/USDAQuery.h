fa563c9af0f897c58a18cd777072bf9df08fcb77
test <lancehirsch@macbook-pro.local>
2018-10-21 12:49:45 +0300

    Initial Commit

---
+++ USDAQuery/USDAQuery.h
@@ -0,0 +1,19 @@
+//
+//  USDAQuery.h
+//  USDAQuery
+//
+//  Created by Lance Hirsch on 10/21/18.
+//  Copyright © 2018 Lance Hirsch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for USDAQuery.
+FOUNDATION_EXPORT double USDAQueryVersionNumber;
+
+//! Project version string for USDAQuery.
+FOUNDATION_EXPORT const unsigned char USDAQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <USDAQuery/PublicHeader.h>
+
+
